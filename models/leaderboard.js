5d2eba8327ccfe70de9dd01caf115103faf7ca32
faizan-20 <faizan.20a@gmail.com>
2023-10-16 20:56:27 +0530

    create leaderboard modle

---
+++ models/leaderboard.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const LeaderboardSchema = new Schema ({
+    player_name: { type: String, required: true },
+    time: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Leaderboard", LeaderboardSchema);
