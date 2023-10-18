1e455de6e83865d5c335076b27a031909fde84f0
faizan-20 <faizan.20a@gmail.com>
2023-10-18 21:57:24 +0530

    added actualTime field

--- controller/LeaderboardController.js
+++ controller/LeaderboardController.js
@@ -2,7 +2,7 @@ const Leaderboard = require('../models/leaderboard');
 const asyncHandler = require('express-async-handler');
 
 exports.player_list = asyncHandler(async (req, res, next) => {
-    const allPlayers = await Leaderboard.find({}).sort({time: 1}).exec();
+    const allPlayers = await Leaderboard.find({}).sort({actualTime: 1}).exec();
     res.json(allPlayers);
 });
 
@@ -10,6 +10,7 @@ exports.player_create = asyncHandler(async (req, res, next) => {
     const player = new Leaderboard({
         player_name: req.body.player_name,
         time: req.body.time,
+        actualTime: req.body.actualTime,
     });
 
     await player.save();
--- models/leaderboard.js
+++ models/leaderboard.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const LeaderboardSchema = new Schema ({
     player_name: { type: String, required: true },
     time: { type: String, required: true },
+    actualTime: { type: Number, required: true },
 });
 
 module.exports = mongoose.model("Leaderboard", LeaderboardSchema);
